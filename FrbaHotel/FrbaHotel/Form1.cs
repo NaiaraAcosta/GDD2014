bd2c5a9e8ec20ebc27a00c2b68312ec9c07ebf0f
gnlaredo <gnlaredo@gmail.com>
2014-10-09 16:59:33 -0300

    Cambios en el gitignore

--- FrbaHotel/FrbaHotel/Form1.cs
+++ FrbaHotel/FrbaHotel/Form1.cs
@@ -13,13 +13,12 @@ namespace FrbaHotel
 {
     public partial class Form1 : Form
     {
-        SqlConnection conn;
+        
         public Form1()
         {
             InitializeComponent();
-        
 
-            string ConnStr = @"Data Source=Gonzalo-PC\SQLSERVER2008;Initial Catalog=GD2C2014;User ID=gd;Password=gd2014;Trusted_Connection=False;";
+            string ConnStr = @"Data Source=localhost\SQLSERVER2008;Initial Catalog=GD2C2014;User ID=gd;Password=gd2014;Trusted_Connection=False;";
 
             SqlConnection conn = new SqlConnection(ConnStr);
             SqlCommand cmd = new SqlCommand("SELECT TOP 10 [Hotel_Ciudad] FROM [GD2C2014].[gd_esquema].[Maestra]", conn);
