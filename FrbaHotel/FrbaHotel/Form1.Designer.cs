7978927943508eabb3688f05dea3a4dac077c488
gnlaredo <gnlaredo@gmail.com>
2014-10-09 18:53:22 -0300

    Nuevo metodo para conectarse a la base
    Nuevo metodo de coneccion que sirve para los datagrid, gitignore
    actualizado otra vez (no quiero los putos obj, ni bin) Nuevas interfaces

--- FrbaHotel/FrbaHotel/Form1.Designer.cs
+++ FrbaHotel/FrbaHotel/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,6 +43,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "LogIn";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // dataGridView1
             // 
@@ -51,7 +53,6 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(240, 150);
             this.dataGridView1.TabIndex = 1;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // listBox1
             // 
@@ -61,11 +62,21 @@
             this.listBox1.Size = new System.Drawing.Size(120, 95);
             this.listBox1.TabIndex = 2;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 320);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(407, 357);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
@@ -74,6 +85,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,5 +94,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- FrbaHotel/FrbaHotel/Form1.cs
+++ FrbaHotel/FrbaHotel/Form1.cs
@@ -9,13 +9,16 @@ using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Configuration;
 
+
 namespace FrbaHotel
 {
     public partial class Form1 : Form
     {
-        
+        private SqlDataAdapter dataAdapter = new SqlDataAdapter();
         public Form1()
         {
+            
+            
             InitializeComponent();
 
             string ConnStr = @"Data Source=localhost\SQLSERVER2008;Initial Catalog=GD2C2014;User ID=gd;Password=gd2014;Trusted_Connection=False;";
@@ -34,19 +37,71 @@ namespace FrbaHotel
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            string sCnn;
+            sCnn = @"data source = Gonzalo-PC\SQLSERVER2008; initial catalog = GD2C2014; user id = gd; password = gd2014";
+
+            string sSel = @"SELECT TOP 1000 [Hotel_Ciudad]
+                                 ,[Hotel_Calle]
+                                 ,[Hotel_Nro_Calle]
+                                 ,[Hotel_CantEstrella]
+                                 ,[Hotel_Recarga_Estrella]
+                                 ,[Habitacion_Numero]
+                                  ,[Habitacion_Piso]
+                                    ,[Habitacion_Frente]
+                                    ,[Habitacion_Tipo_Codigo]
+                                 ,[Habitacion_Tipo_Descripcion]
+      ,[Habitacion_Tipo_Porcentual]
+      ,[Regimen_Descripcion]
+      ,[Regimen_Precio]
+      ,[Reserva_Fecha_Inicio]
+      ,[Reserva_Codigo]
+      ,[Reserva_Cant_Noches]
+      ,[Estadia_Fecha_Inicio]
+      ,[Estadia_Cant_Noches]
+      ,[Consumible_Codigo]
+      ,[Consumible_Descripcion]
+      ,[Consumible_Precio]
+      ,[Item_Factura_Cantidad]
+      ,[Item_Factura_Monto]
+      ,[Factura_Nro]
+      ,[Factura_Fecha]
+      ,[Factura_Total]
+      ,[Cliente_Pasaporte_Nro]
+      ,[Cliente_Apellido]
+      ,[Cliente_Nombre]
+      ,[Cliente_Fecha_Nac]
+      ,[Cliente_Mail]
+      ,[Cliente_Dom_Calle]
+      ,[Cliente_Nro_Calle]
+      ,[Cliente_Piso]
+      ,[Cliente_Depto]
+      ,[Cliente_Nacionalidad]
+  FROM [GD2C2014].[gd_esquema].[Maestra]";
+
+            SqlDataAdapter da;
+            DataTable dt = new DataTable();
+
+            try
+            {
+                da = new SqlDataAdapter(sSel, sCnn);
+                da.Fill(dt);
+
+                this.dataGridView1.DataSource = dt;
+                //this.dataGridView1.DataBind();
+                label1.Text = String.Format("Total datos en la tabla: {0}", dt.Rows.Count);
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "Error: " + ex.Message;
+            }
           
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-
+            Form f = new Login.Form1();
+            f.Show();
         }
-
-       
-
-       
-        
-
+   
     }
 }
