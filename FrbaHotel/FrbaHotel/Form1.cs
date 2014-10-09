28fdafcf63a0808a0c5839d96ca87a562a2f8a12
gnlaredo <gnlaredo@gmail.com>
2014-10-09 16:43:39 -0300

    Agregado proyecto c#, algunas interfaces

---
+++ FrbaHotel/FrbaHotel/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace FrbaHotel
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        public Form1()
+        {
+            InitializeComponent();
+        
+
+            string ConnStr = @"Data Source=Gonzalo-PC\SQLSERVER2008;Initial Catalog=GD2C2014;User ID=gd;Password=gd2014;Trusted_Connection=False;";
+
+            SqlConnection conn = new SqlConnection(ConnStr);
+            SqlCommand cmd = new SqlCommand("SELECT TOP 10 [Hotel_Ciudad] FROM [GD2C2014].[gd_esquema].[Maestra]", conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                listBox1.Items.Add(reader["Hotel_Ciudad"].ToString());
+            }
+            reader.Close();
+            conn.Close(); 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+          
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+       
+
+       
+        
+
+    }
+}
