ab5fe45ead3fe520fdb16b0c97e797ee6ffccff7
NaiaraAcosta <acosta.naiara@gmail.com>
2014-11-04 13:38:08 -0300

    Agregue los GO para que funcione todo de una

--- BASE DE DATOS/04_proc y func.sql
+++ BASE DE DATOS/04_proc y func.sql
@@ -1,5 +1,6 @@
-
+GO
 drop function control_zeta.get_ciudad;
+GO
 create function control_zeta.get_ciudad(@p_Hotel_Ciudad varchar(50))
 returns tinyint
 as
@@ -7,7 +8,9 @@ begin
 return (SELECT LOC_ID FROM CONTROL_ZETA.LOCALIDAD WHERE LOC_DETALLE = @p_Hotel_Ciudad);
 end;
 
+GO
 drop function control_zeta.get_id_hotel;
+GO
 create function control_zeta.get_id_hotel(@p_ciudad VARCHAR(50),
                                           @p_hotel_nro_calle SMALLINT,
                                           @p_hotel_calle   VARCHAR(50) )
@@ -23,8 +26,9 @@ return   (SELECT HOTEL_ID
         AND L.LOC_DETALLE = @p_ciudad);
 end;
 
---drop function control_zeta.get_id_tipo_habitacion;
-
+GO
+drop function control_zeta.get_id_tipo_habitacion;
+GO
 create function control_zeta.get_id_tipo_habitacion(@p_descripcion VARCHAR(50))
 returns smallint
 as
