ebb0c719137dd9d82f5dcfec22df002bc4d2afd2
NaiaraAcosta <acosta.naiara@gmail.com>
2014-12-01 19:03:47 -0300

    CAMBIO EN LOGIN

--- BASE DE DATOS/05_SP_FN_App_Naiu.sql
+++ BASE DE DATOS/05_SP_FN_App_Naiu.sql
@@ -789,7 +789,7 @@ INTO @V_USR ,@V_PASS ,@V_INTENTOS
 
 IF (@@fetch_status=0) 
 BEGIN
-	IF (@V_INTENTOS<4)
+	IF (@V_INTENTOS<3)
 		BEGIN
 			IF(@V_PASS=@pass)
 			begin
@@ -803,7 +803,13 @@ BEGIN
 				SET @error=6 --Pass incorrecta
 			END
 	END
-	ELSE set @error=5 --Ya hizo 3 intentos fallidos
+	ELSE 
+	begin
+		UPDATE CONTROL_ZETA.USR_ROL_HOTEL SET ESTADO='I'
+		WHERE USR_USERNAME=@usr
+		set @error=5 --Ya hizo 3 intentos fallidos
+	end
+	
 END
 ELSE SET @error=2 --No esta el usr
 
